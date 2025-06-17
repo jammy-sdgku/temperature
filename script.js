089ab25261c8356871d2c4b98c3c7aeda82488d4
jammy-sdgku <james.jones@sdgku.edu>
2025-06-17 03:29:32 +0100

    first push

---
+++ script.js
@@ -0,0 +1,58 @@
+ //alerts
+ //    alert("Hello cohort 59");
+        console.log("Hello cohort 59");
+        console.warn("Warning ....cohort 59 is here!");
+        console.error("Error this is bad");
+        console.log("script");
+
+        //variables
+
+
+        //strings
+        let firstName = "James";
+        let lastName = "Jones";
+
+       
+
+
+        //numerical
+        let age = 50;
+        let mySize = 9;
+
+        //boolean values
+        let isStudent = true; 
+        
+        //concatanation
+        console.log("My name is " +firstName+" "+ lastName);
+
+        //operations
+        let num1 = 10;
+        let num2 = 2;
+        let addition = num1 + num2;
+        let subtraction = num1 - num2;
+        let multiplication = num1 * num2;
+        let division = num1 / num2;
+        
+        const pi = 3.14
+        let radius = 3 * 3;
+        let area = pi * radius
+
+        console.log("Addition is: " + addition);
+        console.log("Subtraction is: " + subtraction);
+        console.log("Multiplication is: " + multiplication);
+        console.log("Division is: " + division);
+        console.log("Area is: " + area);
+
+        let aVar = 20;
+        let otherVar = 30;
+        console.log(aVar, otherVar);
+
+        aVar = 50;
+        otherVar = 60;
+        console.log(aVar, otherVar);
+
+        const weekDays = 5;
+        console.log(weekDays);
+        //weekDays = 8; // will error because const can't change. 
+
+        
