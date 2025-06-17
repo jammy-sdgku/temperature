0c2750797b460c1da6e943cf381dc53e5a460369
jammy-sdgku <james.jones@sdgku.edu>
2025-06-17 04:09:16 +0100

    assignment 1

---
+++ assignment1.js
@@ -0,0 +1,32 @@
+
+//variables
+let character = "James";
+let action = "talk";
+let object = "some motorcycle riding friends";
+let place = "the park";
+let time = "a beautiful weekend Saturday";
+
+//const
+const occation = "an awesome group motorcycle ride";
+
+//story
+console.log("---James' Funny Story---");
+console.log(character + " decided to chill at " + place + " on " + time + ".");
+console.log("But then " + character + " noticed " + object + " and waved them over to " + action + ".");
+console.log("The group was really cool so all went on " + occation + " and it turned out to be a great day."); 
+
+
+//new variables
+character = "Batman";
+action = "fight";
+object = "some evil villans";
+place = "Gotham City Playhouse";
+time = "a dark rainy night";
+
+
+//different story
+console.log("---Batmans' Funny Story---");
+console.log(character + " decided to chill at " + place + " on " + time + ".");
+console.log("But then " + character + " noticed " + object + " and waved them over to " + action + ".");
+console.log("The group was really cool so all went on " + occation + " and it turned out to be a great day."); 
+
